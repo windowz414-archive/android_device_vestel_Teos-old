9321ae67d58f7952149d51c6d7484faa101566bb
Wind/owZ <bedirhan.kurt.azan.emirhan.55@gmail.com>
2021-08-20 10:49:20 +0300

    Teos: Wind/owZ x nift4 - Initial push
    This commit was done to initialize the device tree for Teos (Vestel Venus V5/Turk Telekom V5).
    
    Co-authored-by: Nick <nift4@protonmail.com>
    Signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

---
+++ BoardConfig.mk
@@ -0,0 +1,5 @@
+DEVICE_PATH := device/vestel/Teos
+BOARD_VENDOR := vestel
+
+
+-include vendor/vestel/Teos/BoardConfigVendor.mk
---
+++ lineage_Teos.mk
@@ -0,0 +1,21 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit from Teos device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := vestel
+PRODUCT_DEVICE := Teos
+PRODUCT_MANUFACTURER := vestel
+PRODUCT_NAME := lineage_Teos
+PRODUCT_MODEL := Venus V5
+
+TARGET_VENDOR_PRODUCT_NAME := Teos
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="Teos-user 7.1.2 VTE1190 131806 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := vestel/Teos/teos:7.1.2/VTE1190/131806:user/release-keys
