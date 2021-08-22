65d08a6deadeddcfb93d562010bb1da9ea97877e
Wind/owZ <windowz414@gnuweeb.org>
2021-08-23 00:26:44 +0300

    Revert "[EXPERIMENTAL] Teos: vendorsetup: More hacks and little sorting"
    Since it didn't work, the hack has been removed.
    
    This reverts commit 22f5c1a347bc7e953d3593bcd583cbee9a025f17.
    
    Signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,5 @@
 # Little thing to fix issue with prebuilt kernel.
 # This is temporary and is subject to removal.
 mkdir -p out/target/product/Teos/obj/KERNEL_OBJ/usr
-
-# Define LC_ALL to avoid Nougat madness on requiring en_US locale.
 export LC_ALL=C
 
-# Little thing to fix the build error regarding lack of this directory.
-# This is temporary and is subject to removal.
-mkdir -p out/target/product/Teos/system/vendor/CTA/system
